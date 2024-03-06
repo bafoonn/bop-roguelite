360ac3bb7e3ec162fc8aaa8047819d85472aaf45
EinoKa <97962950+EinoKa@users.noreply.github.com>
2024-03-06 17:48:15 +0200

    finaldoor change

--- Assets/Code/MapGeneration/FinalPortal.cs
+++ Assets/Code/MapGeneration/FinalPortal.cs
@@ -6,12 +6,11 @@ namespace Pasta
 {
     public class FinaPortal : MonoBehaviour
     {
-        private bool isActive = false;
 
         // When player collides with an endpoint, level generation is called and the reward for this endpoint is passed to the next level as its reward
         private void OnTriggerEnter2D(Collider2D col)
         {
-            if (col.gameObject.CompareTag("Player") && isActive)
+            if (col.gameObject.CompareTag("Player"))
             {
                 LevelManager levelManager = GetComponentInParent<LevelManager>();
                 if (GetComponentInParent<Region>())
@@ -21,16 +20,6 @@ namespace Pasta
                         HUD.Current.OpenWindow(HUD.Window.Victory, true);
                     });
                 }
-                levelManager.DisableShopKeeper();
-            }
-        }
-
-        // Keeps the player from activating the exit in case the player is inside the endpoint when they are spawned
-        private void OnTriggerExit2D(Collider2D col)
-        {
-            if (col.gameObject.CompareTag("Player") && !isActive)
-            {
-                isActive = true;
             }
         }
     }
